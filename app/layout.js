60b869a0caa45faae0fac9798229caf2d968b295
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-15 08:53:20 +0400

    clerk authentication

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,12 @@
 import { Geist, Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -22,8 +29,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={outfit.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={outfit.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
